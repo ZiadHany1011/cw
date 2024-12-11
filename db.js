c147fcd64c641af377fbe4d4529bd9e9df78bea8
Ziad Hany Mohamed Abdelalim <za2200213@tkh.edu.eg>
2024-12-11 21:13:17 +0200

    create user table

--- db.js
+++ db.js
@@ -1,2 +1,12 @@
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('bitebuilder.db');
+
+// Table creation queries
+const createUserTable = `
+CREATE TABLE IF NOT EXISTS USER (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    NAME TEXT NOT NULL,
+    EMAIL TEXT UNIQUE NOT NULL,
+    PASSWORD TEXT NOT NULL,
+    ISADMIN INT
+)`;
